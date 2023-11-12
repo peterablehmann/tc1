424329cf6dc44d1fe5ede24a7e07c7b67f9215ae
Peter Lehmann <peter@lehmann.de>
2023-11-12 21:22:16 +0100

    Enable additional collectors

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -2,6 +2,10 @@
   services.prometheus.exporters.node = {
       enable = true;
       listenAddress = "192.168.10.10";
+      enabledCollectors = [
+        "systemd"
+        "ethtool"
+      ];
   };
 
 
