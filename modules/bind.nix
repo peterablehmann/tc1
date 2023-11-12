2b2c7eb2af75dd09301044a3074b41a17351ff5d
Peter Lehmann <peter@lehmann.de>
2023-11-12 22:03:48 +0100

    Setup Bind9

---
+++ modules/bind.nix
@@ -0,0 +1,17 @@
+{
+  services.bind = {
+    enable = true;
+
+    forward = "first";
+    forwarders = [
+      "9.9.9.9"
+      "149.112.112.112"
+      "2620:fe::fe"
+      "2620:fe::9"
+    ];
+    cacheNetworks = [
+      "127.0.0.0/8"
+      "192.168.10.0/23"
+    ];
+  };
+}
