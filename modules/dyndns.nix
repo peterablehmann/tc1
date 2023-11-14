a011e5cfb4a8c5d6044e4045e7d04bd21669df72
Peter Lehmann <peter@lehmann.de>
2023-11-14 20:08:36 +0100

    Refactor using systemd timer

--- modules/dyndns.nix
+++ modules/dyndns.nix
@@ -15,15 +15,28 @@
   sops.secrets."environment/recordid" = {
     sopsFile = "${inputs.self}/secrets/environment.yaml";
   };
-  environment.variables = { 
-    apitoken = config.sops.secrets."environment/apitoken".path;
-    zoneid = config.sops.secrets."environment/zoneid".path;
-    recordid = config.sops.secrets."environment/recordid".path;
+  
+  systemd.timers."dyndns" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "1m";
+      OnUnitActiveSec = "1m";
+      Unit = "dyndns.service";
+    };
   };
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "*/1 * * * * [ $(ip -6 a | grep \"scope global dynamic mngtmpaddr noprefixroute\" | grep \"2003:\" | cut -d \" \" -f 6 | sed \'s/.\{3\}$//\') = $(curl -s \"https://dns.hetzner.com/api/v1/records/$recordid\" -H \"Auth-API-Token: $apitoken\" | jq \".record.value\" | tr -d \'\"\') ] || curl -s -X \"PUT\" \"https://dns.hetzner.com/api/v1/records/$recordid\" -H \'Content-Type: application/json\' -H \"Auth-API-Token: $apitoken\" -d $\'{\"value\": \"$(ip -6 a | grep \"scope global dynamic mngtmpaddr noprefixroute\" | grep \"2003:\" | cut -d \" \" -f 6 | sed \'s/.\{3\}$//\')\",\"ttl\": 0,\"type\": \"AAAA\",\"name\": \"tc1\",\"zone_id\": \"$zoneid\"}\'"
-    ];
+
+  systemd.services."dyndns" = {
+    script = ''
+      OWN_IP=$(${pkgs.iproute2}/bin/ip -6 a | grep "scope global dynamic mngtmpaddr noprefixroute" | grep "2003:" | cut -d " " -f 6 | sed 's/.\{3\}$//')
+      DNS_IP=$(${pkgs.curl}/bin/curl -s "https://dns.hetzner.com/api/v1/records/$(cat ${config.sops.secrets."environment/recordid".path})" -H "Auth-API-Token: $(cat ${config.sops.secrets."environment/apitoken".path})" | ${pkgs.jq}/bin/jq ".record.value" | tr -d '"')
+      echo "Current IP: $OWN_IP"
+      echo "DNS IP: $DNS_IP"
+
+      [ $OWN_IP = $DNS_IP ] && echo "Already up to date" || ${pkgs.curl}/bin/curl -s -X "PUT" "https://dns.hetzner.com/api/v1/records/$(cat ${config.sops.secrets."environment/recordid".path})" -H 'Content-Type: application/json' -H "Auth-API-Token: $(cat ${config.sops.secrets."environment/apitoken".path})" -d $'{"value": "'$OWN_IP'", "ttl": 60, "type": "AAAA", "name": "'$HOSTNAME'", "zone_id": "'$(cat ${config.sops.secrets."environment/zoneid".path})'"}'
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
   };
 }
